b9eafd707b626047dc4ddbe1863776a42f0e066e
MiguelABecerraT <80001912+MiguelABecerraT@users.noreply.github.com>
2021-08-05 00:05:40 -0500

    Add files via upload

---
+++ build/precache-manifest.9325e795ff1abd0e0d3b5e4c98396a58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b998388a13370595e81815c438657855",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cabcf65699d3d6bd5360",
+    "url": "/static/css/main.c8b3eeb8.chunk.css"
+  },
+  {
+    "revision": "ccdae4f281fba14798b2",
+    "url": "/static/js/2.3790b743.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.3790b743.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cabcf65699d3d6bd5360",
+    "url": "/static/js/main.8a0a5052.chunk.js"
+  },
+  {
+    "revision": "0d4e3fbca24db45e5036",
+    "url": "/static/js/runtime-main.86719c39.js"
+  }
+]);
